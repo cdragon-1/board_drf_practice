e29171b2384f9fc6376367bb91f9590ce55da576
cdragon-1 <streetfood89@gmail.com>
2017-03-20 00:42:21 +0900

    user_manager와 post_service view 내용 분할, urls경로 분할

--- django_app/post_service/urls.py
+++ django_app/post_service/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 
-from post_service.views import post_list, login, login_validate
+from post_service.views import post_list
 
 urlpatterns = [
     url(r'^$', post_list),
-    url(r'^login/$', login),
-    url(r'^login/validate/$', login_validate),
+
 ]
--- django_app/user_manager/views.py
+++ django_app/user_manager/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.template import Context
+from django.template.context_processors import csrf
+
+from django.template.loader import get_template
+
+from post_service.forms import LoginForm
 
 
+def login(request):
+    template = get_template('login_form.html')
 
-def login():
-    pass
+    context = Context({'login_form': LoginForm()})
+    context.update(csrf(request))
+
+    return HttpResponse(template.render(context))
 
 
-def login_validate():
-    pass
+def login_validate(request):
+    login_form_data = LoginForm(request.POST)
 
+    if login_form_data.is_valid():
+        user = auth.authenticate(username=login_form_data['id'], password=login_form_data['password'])
+        if user is not None:
+            if user.is_active:
+                auth.login(request, user)
+
+                return redirect('/board/')
+        else:
+            return HttpResponse('사용자가 없거나 비밀번호를 잘못 누르셨습니다.')
+    else:
+        return HttpResponse('로그인 폼이 비정상적입니다.')
