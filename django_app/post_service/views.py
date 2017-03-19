331bfdf9dde7ec3c5443beb39315b399dcaa8480
cdragon-1 <streetfood89@gmail.com>
2017-03-19 18:45:14 +0900

    Paginator 구현하기

--- django_app/post_service/views.py
+++ django_app/post_service/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.template import Context
 from django.template.loader import get_template
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+from post_service.models import Post
 
 
 def post_list(request):
     template = get_template('post_list.html')
-    context = Context({})
+
+    page_data = Paginator(Post.objects.all(), 3)
+    page = request.GET.get('page')
+    try:
+        posts = page_data.page(page)
+    except PageNotAnInteger:
+        posts = page_data.page(1)
+    except EmptyPage:
+        posts = page_data.page(page_data.num_pages)
+
+
+    context = Context({'post_list': posts})
 
     return HttpResponse(template.render(context))
-
